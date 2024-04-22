ed12e90876f69c36e239fa5f52e88d4c36dcd8a9
shashikantdev1 <shashikant@aquacodes.com>
2024-04-22 10:09:30 +0530

    tawk chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'testApp';
+  constructor() { }
+  ngOnInit(): void {
+		// Place your code here
+
+	}
 }
